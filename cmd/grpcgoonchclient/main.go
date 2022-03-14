c06031477c5eae9189fa7cca4764283642334164
Katherine Nieman <thaigoonch@gmail.com>
2022-03-14 11:21:41 -0600

    try concurrency

--- cmd/grpcgoonchclient/main.go
+++ cmd/grpcgoonchclient/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"sync"
+	"time"
 
 	grpcgoonch "github.com/thaigoonch/grpcgoonch/service"
 	"golang.org/x/net/context"
@@ -10,34 +12,49 @@ import (
 	"google.golang.org/grpc/grpclog"
 )
 
-func main() {
-	for {
-		port := 9000
-		host := "grpcgoonch-service"
-		opts := []grpc.DialOption{
-			grpc.WithInsecure(),
-			grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`),
-		}
-		conn, err := grpc.Dial(fmt.Sprintf("dns:///%s:%d", host, port), opts...)
-		if err != nil {
-			grpclog.Fatalf("Could not connect on port %d: %v", port, err)
-		}
-		defer conn.Close()
-
-		c := grpcgoonch.NewServiceClient(conn)
-
-		text := "encrypt me"
-		key := []byte("#89er@jdks$jmf_d")
-		request := grpcgoonch.Request{
-			Text: text,
-			Key:  key,
-		}
-
-		response, err := c.CryptoRequest(context.Background(), &request)
-		if err != nil {
-			grpclog.Fatalf("Error when calling CryptoRequest(): %v", err)
-		}
-
-		log.Printf("Response from Goonch Server: %s", response.Result)
+func doClientThings() {
+	port := 9000
+	host := "grpcgoonch-service"
+	opts := []grpc.DialOption{
+		grpc.WithInsecure(),
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`),
 	}
+	conn, err := grpc.Dial(fmt.Sprintf("dns:///%s:%d", host, port), opts...)
+	if err != nil {
+		grpclog.Fatalf("Could not connect on port %d: %v", port, err)
+	}
+	defer conn.Close()
+
+	c := grpcgoonch.NewServiceClient(conn)
+
+	text := "encrypt me"
+	key := []byte("#89er@jdks$jmf_d")
+	request := grpcgoonch.Request{
+		Text: text,
+		Key:  key,
+	}
+
+	response, err := c.CryptoRequest(context.Background(), &request)
+	if err != nil {
+		grpclog.Fatalf("Error when calling CryptoRequest(): %v", err)
+	}
+
+	log.Printf("Response from Goonch Server: %s", response.Result)
+}
+
+func main() {
+	start := time.Now()
+	defer func() {
+		fmt.Println("Execution Time: ", time.Since(start))
+	}()
+	wg := sync.WaitGroup{}
+
+	for {
+		wg.Add(1)
+		go func() {
+			doClientThings()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
