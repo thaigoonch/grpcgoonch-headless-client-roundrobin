9ece2dc3eabf2ad097ea0290e89fa67cc938cf28
Katherine Nieman <thaigoonch@gmail.com>
2022-03-14 12:18:23 -0600

    try finite threads of inf loops

--- cmd/grpcgoonchclient/main.go
+++ cmd/grpcgoonchclient/main.go
@@ -13,33 +13,35 @@ import (
 )
 
 func doClientThings() {
-	port := 9000
-	host := "grpcgoonch-service"
-	opts := []grpc.DialOption{
-		grpc.WithInsecure(),
-		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`),
-	}
-	conn, err := grpc.Dial(fmt.Sprintf("dns:///%s:%d", host, port), opts...)
-	if err != nil {
-		grpclog.Fatalf("Could not connect on port %d: %v", port, err)
-	}
-	defer conn.Close()
+	for {
+		port := 9000
+		host := "grpcgoonch-service"
+		opts := []grpc.DialOption{
+			grpc.WithInsecure(),
+			grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`),
+		}
+		conn, err := grpc.Dial(fmt.Sprintf("dns:///%s:%d", host, port), opts...)
+		if err != nil {
+			grpclog.Fatalf("Could not connect on port %d: %v", port, err)
+		}
+		defer conn.Close()
 
-	c := grpcgoonch.NewServiceClient(conn)
+		c := grpcgoonch.NewServiceClient(conn)
 
-	text := "encrypt me"
-	key := []byte("#89er@jdks$jmf_d")
-	request := grpcgoonch.Request{
-		Text: text,
-		Key:  key,
+		text := "encrypt me"
+		key := []byte("#89er@jdks$jmf_d")
+		request := grpcgoonch.Request{
+			Text: text,
+			Key:  key,
+		}
+
+		response, err := c.CryptoRequest(context.Background(), &request)
+		if err != nil {
+			grpclog.Fatalf("Error when calling CryptoRequest(): %v", err)
+		}
+
+		log.Printf("Response from Goonch Server: %s", response.Result)
 	}
-
-	response, err := c.CryptoRequest(context.Background(), &request)
-	if err != nil {
-		grpclog.Fatalf("Error when calling CryptoRequest(): %v", err)
-	}
-
-	log.Printf("Response from Goonch Server: %s", response.Result)
 }
 
 func main() {
@@ -49,7 +51,7 @@ func main() {
 	}()
 	wg := sync.WaitGroup{}
 
-	for {
+	for i := 0; i < 11; i++ {
 		wg.Add(1)
 		go func() {
 			doClientThings()
