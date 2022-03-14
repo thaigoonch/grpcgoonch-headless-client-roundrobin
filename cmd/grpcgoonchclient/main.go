625eb35fff9792033192d2483eaa35910fe826b3
Katherine Nieman <thaigoonch@gmail.com>
2022-03-14 10:58:56 -0600

    forgot to save

--- cmd/grpcgoonchclient/main.go
+++ cmd/grpcgoonchclient/main.go
@@ -11,32 +11,33 @@ import (
 )
 
 func main() {
+	for {
+		port := 9000
+		host := "grpcgoonch-service"
+		opts := []grpc.DialOption{
+			grpc.WithInsecure(),
+			grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`),
+		}
+		conn, err := grpc.Dial(fmt.Sprintf("dns:///%s:%d", host, port), opts...)
+		if err != nil {
+			grpclog.Fatalf("Could not connect on port %d: %v", port, err)
+		}
+		defer conn.Close()
 
-	port := 9000
-	host := "grpcgoonch-service"
-	opts := []grpc.DialOption{
-		grpc.WithInsecure(),
-		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`),
+		c := grpcgoonch.NewServiceClient(conn)
+
+		text := "encrypt me"
+		key := []byte("#89er@jdks$jmf_d")
+		request := grpcgoonch.Request{
+			Text: text,
+			Key:  key,
+		}
+
+		response, err := c.CryptoRequest(context.Background(), &request)
+		if err != nil {
+			grpclog.Fatalf("Error when calling CryptoRequest(): %v", err)
+		}
+
+		log.Printf("Response from Goonch Server: %s", response.Result)
 	}
-	conn, err := grpc.Dial(fmt.Sprintf("dns:///%s:%d", host, port), opts...)
-	if err != nil {
-		grpclog.Fatalf("Could not connect on port %d: %v", port, err)
-	}
-	defer conn.Close()
-
-	c := grpcgoonch.NewServiceClient(conn)
-
-	text := "encrypt me"
-	key := []byte("#89er@jdks$jmf_d")
-	request := grpcgoonch.Request{
-		Text: text,
-		Key:  key,
-	}
-
-	response, err := c.CryptoRequest(context.Background(), &request)
-	if err != nil {
-		grpclog.Fatalf("Error when calling CryptoRequest(): %v", err)
-	}
-
-	log.Printf("Response from Goonch Server: %s", response.Result)
 }
