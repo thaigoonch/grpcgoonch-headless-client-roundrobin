4b0b72526f1a91d3aaad748ff6f0d7de0e964026
Katherine Nieman <thaigoonch@gmail.com>
2022-04-07 10:09:46 -0600

    move http server creation into main

--- cmd/grpcgoonchclient/main.go
+++ cmd/grpcgoonchclient/main.go
@@ -21,23 +21,7 @@ var (
 	promPort = 9095
 )
 
-func doClientThings() {
-	reg := prometheus.NewRegistry()
-	grpcMetrics := grpc_prometheus.NewClientMetrics()
-	reg.MustRegister(grpcMetrics)
-
-	// Create an http server for prometheus
-	httpServer := &http.Server{
-		Handler: promhttp.HandlerFor(reg, promhttp.HandlerOpts{}),
-		Addr:    fmt.Sprintf(":%d", promPort)}
-
-	// Start http server for prometheus
-	go func() {
-		if err := httpServer.ListenAndServe(); err != nil {
-			log.Fatalf("Error starting http server: %v", err)
-		}
-	}()
-
+func doClientThings(grpcMetrics *grpc_prometheus.ClientMetrics) {
 	for i := 0; i < 100; i++ {
 		host := "grpcgoonch-service"
 		opts := []grpc.DialOption{
@@ -76,12 +60,29 @@ func main() {
 	defer func() {
 		fmt.Println("Execution Time: ", time.Since(start))
 	}()
+
+	reg := prometheus.NewRegistry()
+	grpcMetrics := grpc_prometheus.NewClientMetrics()
+	reg.MustRegister(grpcMetrics)
+
+	// Create an http server for prometheus
+	httpServer := &http.Server{
+		Handler: promhttp.HandlerFor(reg, promhttp.HandlerOpts{}),
+		Addr:    fmt.Sprintf(":%d", promPort)}
+
+	// Start http server for prometheus
+	go func() {
+		if err := httpServer.ListenAndServe(); err != nil {
+			log.Fatalf("Error starting http server: %v", err)
+		}
+	}()
+
 	wg := sync.WaitGroup{}
 
 	for i := 0; i < 11; i++ {
 		wg.Add(1)
 		go func() {
-			doClientThings()
+			doClientThings(grpcMetrics)
 			wg.Done()
 		}()
 	}
