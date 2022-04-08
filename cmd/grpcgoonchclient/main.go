22588c312bbad288a79fe4c0e3d244b7f9f2ff54
Katherine Nieman <thaigoonch@gmail.com>
2022-04-07 21:00:10 -0600

    remove multiple servers

--- cmd/grpcgoonchclient/main.go
+++ cmd/grpcgoonchclient/main.go
@@ -20,38 +20,6 @@ var (
 	promPort = 9095
 )
 
-func doClientThings(grpcMetrics *grpc_prometheus.ClientMetrics) {
-	for i := 0; i < 100; i++ {
-		host := "grpcgoonch-service"
-		opts := []grpc.DialOption{
-			grpc.WithInsecure(),
-			grpc.WithBalancerName("round_robin"),
-			grpc.WithUnaryInterceptor(grpcMetrics.UnaryClientInterceptor()),
-		}
-		conn, err := grpc.Dial(fmt.Sprintf("dns:///%s:%d", host, port), opts...)
-		if err != nil {
-			grpclog.Fatalf("Could not connect on port %d: %v", port, err)
-		}
-		defer conn.Close()
-
-		c := grpcgoonch.NewServiceClient(conn)
-
-		text := "encrypt me"
-		key := []byte("#89er@jdks$jmf_d")
-		request := grpcgoonch.Request{
-			Text: text,
-			Key:  key,
-		}
-
-		response, err := c.CryptoRequest(context.Background(), &request)
-		if err != nil {
-			grpclog.Fatalf("Error when calling CryptoRequest(): %v", err)
-		}
-
-		log.Printf("Response from Goonch Server: %s", response.Result)
-	}
-}
-
 func main() {
 	reg := prometheus.NewRegistry()
 	grpcMetrics := grpc_prometheus.NewClientMetrics()
@@ -69,7 +37,33 @@ func main() {
 		}
 	}()
 
-	for i := 0; i < 11; i++ {
-		doClientThings(grpcMetrics)
+	host := "grpcgoonch-service"
+	opts := []grpc.DialOption{
+		grpc.WithInsecure(),
+		grpc.WithBalancerName("round_robin"),
+		grpc.WithUnaryInterceptor(grpcMetrics.UnaryClientInterceptor()),
+	}
+	conn, err := grpc.Dial(fmt.Sprintf("dns:///%s:%d", host, port), opts...)
+	if err != nil {
+		grpclog.Fatalf("Could not connect on port %d: %v", port, err)
+	}
+	defer conn.Close()
+
+	c := grpcgoonch.NewServiceClient(conn)
+	text := "encrypt me"
+	key := []byte("#89er@jdks$jmf_d")
+	request := grpcgoonch.Request{
+		Text: text,
+		Key:  key,
+	}
+	for i := 0; i < 24; i++ {
+		for i := 0; i < 200; i++ {
+			response, err := c.CryptoRequest(context.Background(), &request)
+			if err != nil {
+				grpclog.Fatalf("Error when calling CryptoRequest(): %v", err)
+			}
+
+			log.Printf("Response from Goonch Server: %s", response.Result)
+		}
 	}
 }
